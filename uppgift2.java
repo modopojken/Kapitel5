f9fd38a804fcbfe2e009b3bf10b732e303fc7b4a
coel16 <modopojken@gmail.com>
2017-10-09 09:48:40 +0200

    uppgift2

---
+++ uppgift2.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class uppgift2{
+	public static void main(String[]args){
+String n;
+double i = 1;
+double sum = 0;
+n = JOptionPane.showInputDialog(null, "Skriv in nummer");
+double ns = Double.parseDouble(n);
+while(i <= ns){
+sum = sum + 1 / i;
+i++;
+
+ 
+	
+	}
+
+	JOptionPane.showMessageDialog(null, "Summan är: " + sum);
+	}
+}
